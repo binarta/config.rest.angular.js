788d61c77b44e333f527c972af268c01041d8a48
Jo Kerkhofs <jke@thinkerit.be>
2015-05-15 11:45:43 +0200

    cleanup spec

--- test/config.rest.spec.js
+++ test/config.rest.spec.js
@@ -3,114 +3,112 @@ describe('config.rest', function () {
     beforeEach(module('config'));
     beforeEach(module('config.gateways'));
 
-    describe('publicConfigReader', function () {
-        var configReader, publicConfigReader, configWriter, publicConfigWriter, request, response, value;
+    var configReader, publicConfigReader, configWriter, publicConfigWriter, request, response, value;
 
-        beforeEach(inject(function (_configReader_, _publicConfigReader_, _configWriter_, _publicConfigWriter_) {
-            configReader = _configReader_;
-            publicConfigReader = _publicConfigReader_;
-            configWriter = _configWriter_;
-            publicConfigWriter = _publicConfigWriter_;
-            value = undefined;
-        }));
+    beforeEach(inject(function (_configReader_, _publicConfigReader_, _configWriter_, _publicConfigWriter_) {
+        configReader = _configReader_;
+        publicConfigReader = _publicConfigReader_;
+        configWriter = _configWriter_;
+        publicConfigWriter = _publicConfigWriter_;
+        value = undefined;
+    }));
 
-        describe('reading', function () {
-            describe('with response', function () {
-                beforeEach(function () {
-                    request = {
-                        key: 'x'
-                    };
-                    response = {
-                        success: function (it) {
-                            value = it
-                        }
-                    };
-                    publicConfigReader(request, response);
-                });
-
-                it('delegates to configReader', function () {
-                    expect(configReader.calls[0].args[0].$scope).toEqual({});
-                    expect(configReader.calls[0].args[0].key).toEqual('x');
-                    expect(configReader.calls[0].args[0].scope).toEqual('public');
-                });
-
-                it('known values', function () {
-                    configReader.calls[0].args[0].success('a');
-                    expect(value).toEqual('a');
-                });
+    describe('reading', function () {
+        describe('with response', function () {
+            beforeEach(function () {
+                request = {
+                    key: 'x'
+                };
+                response = {
+                    success: function (it) {
+                        value = it
+                    }
+                };
+                publicConfigReader(request, response);
             });
 
-            describe('without response', function () {
-                beforeEach(function () {
-                    request = {
-                        key: 'x'
-                    };
+            it('delegates to configReader', function () {
+                expect(configReader.calls[0].args[0].$scope).toEqual({});
+                expect(configReader.calls[0].args[0].key).toEqual('x');
+                expect(configReader.calls[0].args[0].scope).toEqual('public');
+            });
 
-                    publicConfigReader(request);
-                });
-
-                it('delegates to configReader', function () {
-                    expect(configReader.calls[0].args[0].$scope).toEqual({});
-                    expect(configReader.calls[0].args[0].key).toEqual('x');
-                    expect(configReader.calls[0].args[0].scope).toEqual('public');
-                });
-
-                it('value is unknown', function () {
-                    expect(value).toBeUndefined();
-                });
+            it('known values', function () {
+                configReader.calls[0].args[0].success('a');
+                expect(value).toEqual('a');
             });
         });
 
+        describe('without response', function () {
+            beforeEach(function () {
+                request = {
+                    key: 'x'
+                };
 
-        describe('writing', function () {
-            describe('with response', function () {
-                beforeEach(function () {
-                    request = {
-                        key: 'x',
-                        value: 'a'
-                    };
-                    response = {
-                        success: function (it) {
-                            value = it
-                        }
-                    };
-                    publicConfigWriter(request, response);
-                });
-
-                it('delegates to configWriter', function () {
-                    expect(configWriter.calls[0].args[0].$scope).toEqual({});
-                    expect(configWriter.calls[0].args[0].key).toEqual('x');
-                    expect(configWriter.calls[0].args[0].value).toEqual('a');
-                    expect(configWriter.calls[0].args[0].scope).toEqual('public');
-                });
-
-                it('write accepted', function () {
-                    configWriter.calls[0].args[0].success();
-
-                    expect(value).toEqual('a');
-                });
+                publicConfigReader(request);
             });
 
-            describe('without response', function () {
-                beforeEach(function () {
-                    request = {
-                        key: 'x',
-                        value: 'a'
-                    };
+            it('delegates to configReader', function () {
+                expect(configReader.calls[0].args[0].$scope).toEqual({});
+                expect(configReader.calls[0].args[0].key).toEqual('x');
+                expect(configReader.calls[0].args[0].scope).toEqual('public');
+            });
 
-                    publicConfigWriter(request);
-                });
+            it('value is unknown', function () {
+                expect(value).toBeUndefined();
+            });
+        });
+    });
 
-                it('delegates to configWriter', function () {
-                    expect(configWriter.calls[0].args[0].$scope).toEqual({});
-                    expect(configWriter.calls[0].args[0].key).toEqual('x');
-                    expect(configWriter.calls[0].args[0].value).toEqual('a');
-                    expect(configWriter.calls[0].args[0].scope).toEqual('public');
-                });
 
-                it('value is unknown', function () {
-                    expect(value).toBeUndefined();
-                });
+    describe('writing', function () {
+        describe('with response', function () {
+            beforeEach(function () {
+                request = {
+                    key: 'x',
+                    value: 'a'
+                };
+                response = {
+                    success: function (it) {
+                        value = it
+                    }
+                };
+                publicConfigWriter(request, response);
+            });
+
+            it('delegates to configWriter', function () {
+                expect(configWriter.calls[0].args[0].$scope).toEqual({});
+                expect(configWriter.calls[0].args[0].key).toEqual('x');
+                expect(configWriter.calls[0].args[0].value).toEqual('a');
+                expect(configWriter.calls[0].args[0].scope).toEqual('public');
+            });
+
+            it('write accepted', function () {
+                configWriter.calls[0].args[0].success();
+
+                expect(value).toEqual('a');
+            });
+        });
+
+        describe('without response', function () {
+            beforeEach(function () {
+                request = {
+                    key: 'x',
+                    value: 'a'
+                };
+
+                publicConfigWriter(request);
+            });
+
+            it('delegates to configWriter', function () {
+                expect(configWriter.calls[0].args[0].$scope).toEqual({});
+                expect(configWriter.calls[0].args[0].key).toEqual('x');
+                expect(configWriter.calls[0].args[0].value).toEqual('a');
+                expect(configWriter.calls[0].args[0].scope).toEqual('public');
+            });
+
+            it('value is unknown', function () {
+                expect(value).toBeUndefined();
             });
         });
     });
